b8e869106dc92fc1693ba8c24f7c695721930bfc
Dennis <kilonzod19@gmail.com>
2025-11-22 07:08:09 +0300

    Fixed duplicate length fields

--- src/dtos.rs
+++ src/dtos.rs
@@ -13,10 +13,7 @@ pub struct RegisteredUserDto {
         email(message = "Invalid email format")
     )]
     pub email: String,
-    #[validate(
-        length(min = 1, message = "Password is required"),
-        length(min = 6, message = "Password must be at least 6 characters long")
-    )]
+    #[validate(length(min = 6, message = "Password must be at least 6 characters long"))]
     pub password: String,
     #[validate(
         length(min=1, message = "Confirm Password is required"),
@@ -29,10 +26,7 @@ pub struct RegisteredUserDto {
 pub struct LoginUserDto {
     #[validate(length(min = 1, message = "Email is required"), email(message = "Email is invalid"))]
     pub email: String,
-    #[validate(
-        length(min = 1, message = "Password is required"),
-        length(min = 6, message = "Password must be at least 6 characters")
-    )]
+    #[validate(length(min = 6, message = "Password must be at least 6 characters"))]
     pub password: String,
 }
 
@@ -123,7 +117,6 @@ pub struct UserPasswordUpdateDto {
     #[validate(length(min = 1, message = "New password is required"))]
     pub new_password: String,
     #[validate(
-        length(min = 1, message = "New password confirm is required."),
         length(
             min = 6,
             message = "new password confirm must be at least 6 characters"
@@ -132,10 +125,7 @@ pub struct UserPasswordUpdateDto {
     )]
     pub new_password_confirm: String,
 
-    #[validate(
-        length(min = 1, message = "Old password is required."),
-        length(min = 6, message = "Old password must be at least 6 characters")
-    )]
+    #[validate(length(min = 6, message = "Old password must be at least 6 characters"))]
     pub old_password: String,
 }
 
@@ -160,16 +150,14 @@ pub struct ResetPasswordRequestDto {
     pub token: String,
 
     #[validate(
-        length(min = 1, message = "New password is required."),
         length(min = 6, message = "new password must be at least 6 characters")
     )]
     pub new_password: String,
 
     #[validate(
-        length(min = 1, message = "New password confirm is required."),
         length(
             min = 6,
-            message = "new password confirm must be at least 6 characters"
+            message = "New password confirm must be at least 6 characters"
         ),
         must_match(other = "new_password", message = "new passwords do not match")
     )]
