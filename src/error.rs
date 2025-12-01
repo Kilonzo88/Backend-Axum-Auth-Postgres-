020865ecde7974f4857ef050228e37d133bb4976
Dennis <kilonzod19@gmail.com>
2025-12-01 07:06:26 +0300

    Refactor and added that line to fix a mismatch error on auth.rs where it expected a String but found the field enum 'Http::ErrorMessage'

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,10 @@
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
-    Json
+    Json,
 };
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::fmt;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -26,7 +26,9 @@ pub enum ErrorMessage {
     TokenNotProvided,
     PermissionDenied,
     UserNotAuthenticated,
-    InvalidHashFormat, // Added this variant
+    InvalidHashFormat, 
+    InternalServerError(String),
+    BadRequest(String),
 }
 
 impl fmt::Display for ErrorMessage {
@@ -35,15 +37,29 @@ impl fmt::Display for ErrorMessage {
             ErrorMessage::ServerError => write!(f, "Server Error. Please try again later"),
             ErrorMessage::WrongCredentials => write!(f, "Email or password is wrong"),
             ErrorMessage::EmailExists => write!(f, "A user with this email already exists"),
-            ErrorMessage::UserNoLongerExists => write!(f, "User belonging to this token no longer exists"),
+            ErrorMessage::UserNoLongerExists => {
+                write!(f, "User belonging to this token no longer exists")
+            }
             ErrorMessage::EmptyPassword => write!(f, "Password cannot be empty"),
             ErrorMessage::HashingError => write!(f, "Error while hashing password"),
             ErrorMessage::InvalidHashFormat => write!(f, "Invalid password hash format"),
-            ErrorMessage::ExceededMaxPasswordLength(max_length) => write!(f, "Password must not be more than {} characters", max_length),
+            ErrorMessage::ExceededMaxPasswordLength(max_length) => write!(
+                f,
+                "Password must not be more than {} characters",
+                max_length
+            ),
             ErrorMessage::InvalidToken => write!(f, "Authentication token is invalid or expired"),
-            ErrorMessage::TokenNotProvided => write!(f, "You are not logged in, please provide a token"),
-            ErrorMessage::PermissionDenied => write!(f, "You are not allowed to perform this action"),
-            ErrorMessage::UserNotAuthenticated => write!(f, "Authentication required. Please log in."),
+            ErrorMessage::TokenNotProvided => {
+                write!(f, "You are not logged in, please provide a token")
+            }
+            ErrorMessage::PermissionDenied => {
+                write!(f, "You are not allowed to perform this action")
+            }
+            ErrorMessage::UserNotAuthenticated => {
+                write!(f, "Authentication required. Please log in.")
+            },
+            ErrorMessage::InternalServerError(s) => write!(f, "Internal Server Error: {}", s),
+            ErrorMessage::BadRequest(s) => write!(f, "Bad Request: {}", s),
         }
     }
 }
@@ -56,23 +72,32 @@ pub struct HttpError {
 
 impl HttpError {
     pub fn new(message: ErrorMessage, status_code: StatusCode) -> Self {
-        HttpError { message, status_code }
+        HttpError {
+            message,
+            status_code,
+        }
     }
 
-    pub fn server_error(message: ErrorMessage) -> Self {
-        HttpError::from(message)
+    pub fn server_error<T: Into<String>>(message: T) -> Self {
+        HttpError::new(
+            ErrorMessage::InternalServerError(message.into()),
+            StatusCode::INTERNAL_SERVER_ERROR,
+        )
     }
 
-    pub fn bad_request(message: ErrorMessage) -> Self {
-        HttpError::from(message)
+    pub fn bad_request<T: Into<String>>(message: T) -> Self {
+        HttpError::new(
+            ErrorMessage::BadRequest(message.into()),
+            StatusCode::BAD_REQUEST,
+        )
     }
 
     pub fn unique_constraint_violation(message: ErrorMessage) -> Self {
-        HttpError::from(message)
+        HttpError::new(message, StatusCode::CONFLICT)
     }
 
     pub fn unauthorized(message: ErrorMessage) -> Self {
-        HttpError::from(message)
+        HttpError::new(message, StatusCode::UNAUTHORIZED)
     }
 
     pub fn into_http_response(self) -> Response {
@@ -87,7 +112,11 @@ impl HttpError {
 
 impl fmt::Display for HttpError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "HttpError: message: {}, status: {}", self.status_code, self.message)
+        write!(
+            f,
+            "HttpError: message: {}, status: {}",
+            self.status_code, self.message
+        )
     }
 }
 
@@ -114,8 +143,15 @@ impl From<ErrorMessage> for HttpError {
             ErrorMessage::PermissionDenied => StatusCode::FORBIDDEN,
             ErrorMessage::UserNotAuthenticated => StatusCode::UNAUTHORIZED,
             ErrorMessage::InvalidHashFormat => StatusCode::INTERNAL_SERVER_ERROR, // Assuming this is a server-side issue
+            ErrorMessage::InternalServerError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ErrorMessage::BadRequest(_) => StatusCode::BAD_REQUEST,
         };
         HttpError::new(error_message, status_code)
     }
 }
 
+impl Into<String> for ErrorMessage {
+    fn into(self) -> String {
+        self.to_string()
+    }
+}
