9145499dc58b78bdc403bd7b376829b4916615a4
Dennis <kilonzod19@gmail.com>
2025-12-02 09:48:29 +0300

    First commit of reset_password function

--- src/handler/auth.rs
+++ src/handler/auth.rs
@@ -341,8 +341,46 @@ pub async fn forgot_password(
 }
 
 pub async fn reset_password(
-    State(_app_state): State<Arc<AppState>>,
-    Json(_body): Json<ResetPasswordRequestDto>,
+    State(app_state): State<Arc<AppState>>,
+    Json(body): Json<ResetPasswordRequestDto>,
 ) -> Result<impl IntoResponse, HttpError> {
-    Ok((StatusCode::OK, "Password reset successful"))
+    body.validate()
+        .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+    let result = app_state.db_client
+        .get_user(None, None, None, Some(&body.token))
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    let user = result.ok_or(HttpError::bad_request("Invalid or expired token".to_string()))?;
+
+    if let Some(expires_at) = user.token_expires_at {
+        if Utc::now() > expires_at {
+            return Err(HttpError::bad_request("Verification token has expired".to_string()))?;
+        }
+    }else {
+        return Err(HttpError::bad_request("Invalid verification token".to_string()))?;
+    }
+
+    let user_id = uuid::Uuid::parse_str(&user.id.to_string()).unwrap();
+
+    let hash_password = password::hash(&body.new_password)
+            .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    app_state.db_client
+        .update_user_password(user_id.clone(), hash_password)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    app_state.db_client
+        .verifed_token(&body.token)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    let response = Response {
+        message: "Password has been successfully reset.".to_string(),
+        status: "success",
+    };
+
+    Ok(Json(response))
 }
